6f3a536b7987f0ebf1d6763d91dce653d95449ee
yeun2efan <pianosheetye@gmail.com>
2021-03-18 20:03:22 +0900

    fix
    사진 약간 잘리는 부분, 끝나고 나오는 환호 소리 수정, 아찌 링크 수정

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -35,7 +35,7 @@ let fruitSlowDown = false;
 let clickChangeFruit = true;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/yupi_wechat.png';
+let adLink = 'https://www.twitch.tv/yeun2e';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
 document.getElementsByTagName("title")[0].innerText = '짱옌버전 수박 게임';
